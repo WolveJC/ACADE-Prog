acd9898b7ffec34e0ffc2f4217d8fe5340928714
WolveJC <202882572+WolveJC@users.noreply.github.com>
2025-10-15 16:46:00 -0400

    Actualizar skills.js

--- Wolves-Page/src/data/skills.js
+++ Wolves-Page/src/data/skills.js
@@ -1,1 +1,48 @@
-js
+import { FaPython, FaJava, FaJsSquare, FaHtml5, FaCss3Alt } from 'react-icons/fa';
+import { SiCplusplus } from 'react-icons/si';
+
+// Definimos la información completa de la habilidad
+export const skillsData = [
+    { 
+        name: 'Python', 
+        Icon: FaPython, 
+        percentage: 54, 
+        iconColor: 'text-yellow-400', 
+        barColor: 'bg-yellow-500' // Barra color amarillo
+    },
+    { 
+        name: 'C++', 
+        Icon: SiCplusplus, 
+        percentage: 22, 
+        iconColor: 'text-blue-500', 
+        barColor: 'bg-orange-500' // Barra color naranja 
+    },
+    { 
+        name: 'JavaScript', 
+        Icon: FaJsSquare, 
+        percentage: 10, 
+        iconColor: 'text-yellow-400', 
+        barColor: 'bg-red-600' // Barra color rojo
+    },
+    { 
+        name: 'Java', 
+        Icon: FaJava, 
+        percentage: 5, 
+        iconColor: 'text-red-500', 
+        barColor: 'bg-red-600' // Barra color rojo
+    },
+    { 
+        name: 'HTML5', 
+        Icon: FaHtml5, 
+        percentage: 5, 
+        iconColor: 'text-orange-500', 
+        barColor: 'bg-red-600' // Barra color rojo
+    },
+    { 
+        name: 'CSS3', 
+        Icon: FaCss3Alt, 
+        percentage: 4, 
+        iconColor: 'text-blue-600', 
+        barColor: 'bg-red-600' // Barra color rojo
+    },
+];

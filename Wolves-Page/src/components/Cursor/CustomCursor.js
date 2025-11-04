970aa9f6056e06d6027370275f4cfd2dc25049e2
WolveJC <202882572+WolveJC@users.noreply.github.com>
2025-11-04 17:32:23 -0400

    Refactor cursor styles by removing transform properties
    Removed unnecessary transform properties from cursor styles.

--- Wolves-Page/src/components/Cursor/CustomCursor.js
+++ Wolves-Page/src/components/Cursor/CustomCursor.js
@@ -88,7 +88,6 @@ const CustomCursor = () => {
         <div 
             className={`
                 fixed top-0 left-0 pointer-events-none transition-all duration-300 ease-out z-[9999]
-                transform -translate-x-1/2 -translate-y-1/2
                 ${sizeClass}
                 ${themeClass}
             `}

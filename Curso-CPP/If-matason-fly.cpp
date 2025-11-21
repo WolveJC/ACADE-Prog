10e98e9822be0256c5f0760bbc3d0e8fe1ea3f4a
WolveJC <202882572+WolveJC@users.noreply.github.com>
2025-11-21 10:02:52 -0400

    Refactor If-matason-fly.cpp

--- Curso-CPP/If-matason-fly.cpp
+++ Curso-CPP/If-matason-fly.cpp
@@ -2,130 +2,75 @@
 #include <fstream>
 using namespace std;
 
-struct fly{
-int code;
-float km;
-int num;
+struct Fly {
+    int code;   // Código del vuelo
+    float km;   // Kilómetros recorridos
+    int num;    // Número de pasajeros
 };
 
-void recibocode(float vuela){
+// Genera archivo con el recorrido del vuelo con más pasajeros
+void reciboPasajeros(float km) {
     string filename;
-    string ext;
-    fstream recibo;
-    ext=".txt";
-    cout << "Ingrese el nombre con el que desea guardar el archivo para el recibo del recorrido con mas pasajeros: " << endl;
+    cout << "Ingrese el nombre para guardar el archivo del vuelo con más pasajeros: ";
     cin >> filename;
-    filename+=ext;
-    recibo.open(filename.c_str(),ios::out);
-    if (recibo.fail()) {
-        cout << "* ERROR GENERARANDO RECIBO *" << endl;
+    filename += ".txt";
+
+    ofstream recibo(filename);
+    if (!recibo) {
+        cout << "* ERROR GENERANDO RECIBO *" << endl;
+        return;
     }
-    recibo << "El vuelo con mayor pasajeros recorrío: " << vuela << endl;
-    cout << "Archivo Generado" << endl;
-    recibo.close();
-}
-    
-void recibokm(int vuela){
-    string filename;
-    fstream recibo;
-    string ext;
-    ext=".txt";
-    cout << "Ingrese el nombre con el que desea guardar el archivo para el recibo del codigo con mayor recorrido: " << endl;
-    cin >> filename;
-    filename+=ext;
-    recibo.open(filename.c_str(),ios::out);
-    if (recibo.fail()) {
-        cout << "* ERROR GENERARANDO RECIBO *" << endl;
-    }
-    recibo << "El vuelo con mayor pasajeros recorrío: " << vuela << endl;
-    cout << "Archivo Generado" << endl;
-    recibo.close();
+    recibo << "El vuelo con más pasajeros recorrió: " << km << " km" << endl;
+    cout << "Archivo generado: " << filename << endl;
 }
 
-int main (){
-	int i;
-	i=1;
-	fly vuelo [i];
- for(i=1; i<=5; i++){
- 	cout << "Ingrese codigo del vuelo " << i << endl;
- 	cin>> vuelo[i].code;
-	 cout << "Ingrese el numero de pasajeros del vuelo " << i <<endl;
-	 cin >> vuelo[i].num;
- 	cout << "Ingrese los kilometros que recorrera el vuelo " << i <<endl;
- 	cin >> vuelo[i].km;
- }
- if (vuelo[1].km>vuelo[2].km){
- 	if (vuelo[1].km > vuelo[3].km){
- 		if (vuelo[1].km > vuelo[4].km){
- 			if (vuelo[1].km > vuelo[5].km){
- 			int vuela;
- 				vuela=vuelo[1].code;
- 				recibokm (vuela);
- 	
-			 }
-		 }
-	 }
-}else;
-if (vuelo[2].km>vuelo[3].km){
- 	if (vuelo[2].km > vuelo[4].km){
- 		if (vuelo[2].km > vuelo[5].km){
- 			int vuela;
- 				vuela=vuelo[2].code;
- 				recibokm (vuela);
-		 }
-	 }
-}else;
-if (vuelo[3].km>vuelo[4].km){
- 	if (vuelo[3].km > vuelo[5].km){
- 		int vuela;
- 			vuela=vuelo[3].code;
- 			recibokm (vuela);
-		 }
-	 }else;
-	 if (vuelo[4].km>vuelo[5].km){
- 		int vuela;
- 			vuela=vuelo[3].code;
- 			recibokm (vuela);
-	 }else;
-	 	int vuela;
-	 		vuela=vuelo[5].code;
- 			recibokm (vuela);
-if (vuelo[1].num>vuelo[2].num){
- 	if (vuelo[1].num > vuelo[3].num){
- 		if (vuelo[1].num > vuelo[4].num){
- 			if (vuelo[1].num > vuelo[5].num){
- 			int vuele;
- 				vuele=vuelo[1].km;
- 				recibocode (vuele);
- 	
-			 }
-		 }
-	 }
-}else;
-if (vuelo[2].num>vuelo[3].num){
- 	if (vuelo[2].num > vuelo[4].num){
- 		if (vuelo[2].num > vuelo[5].num){
- 			int vuele;
- 				vuela=vuelo[2].num;
- 				recibocode (vuele);
-		 }
-	 }
-}else;
-if (vuelo[3].num>vuelo[4].num){
- 	if (vuelo[3].num > vuelo[5].num){
- 		int vuele;
- 			vuele=vuelo[3].km;
- 			recibocode (vuele);
-		 }
-	 }else;
-	 if (vuelo[4].num>vuelo[5].num){
- 		int vuele;
- 			vuele=vuelo[4].km;
- 			recibocode (vuela);
-	 }else;
-	 	int vuele;
-	 		vuele=vuelo[5].km;
- 			recibocode (vuele);
+// Genera archivo con el código del vuelo con mayor recorrido
+void reciboKilometros(int code) {
+    string filename;
+    cout << "Ingrese el nombre para guardar el archivo del vuelo con mayor recorrido: ";
+    cin >> filename;
+    filename += ".txt";
 
-return 0;
+    ofstream recibo(filename);
+    if (!recibo) {
+        cout << "* ERROR GENERANDO RECIBO *" << endl;
+        return;
+    }
+    recibo << "El vuelo con mayor recorrido tiene el código: " << code << endl;
+    cout << "Archivo generado: " << filename << endl;
+}
+
+int main() {
+    const int N = 5; // número de vuelos
+    Fly vuelos[N];
+
+    // Entrada de datos
+    for (int i = 0; i < N; i++) {
+        cout << "Ingrese código del vuelo " << i+1 << ": ";
+        cin >> vuelos[i].code;
+        cout << "Ingrese número de pasajeros del vuelo " << i+1 << ": ";
+        cin >> vuelos[i].num;
+        cout << "Ingrese kilómetros del vuelo " << i+1 << ": ";
+        cin >> vuelos[i].km;
+    }
+
+    // Buscar vuelo con mayor recorrido
+    int idxMaxKm = 0;
+    for (int i = 1; i < N; i++) {
+        if (vuelos[i].km > vuelos[idxMaxKm].km) {
+            idxMaxKm = i;
+        }
+    }
+    reciboKilometros(vuelos[idxMaxKm].code);
+
+    // Buscar vuelo con mayor número de pasajeros
+    int idxMaxPasajeros = 0;
+    for (int i = 1; i < N; i++) {
+        if (vuelos[i].num > vuelos[idxMaxPasajeros].num) {
+            idxMaxPasajeros = i;
+        }
+    }
+    reciboPasajeros(vuelos[idxMaxPasajeros].km);
+
+    return 0;
 }
